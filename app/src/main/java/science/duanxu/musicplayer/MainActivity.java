827af00321dc1d2f584f71665c4f83f972faba94
doodle777 <duanxu@outlook.com>
2016-05-25 10:00:23 +0800

    initial commit

--- app/src/main/java/science/duanxu/musicplayer/MainActivity.java
+++ app/src/main/java/science/duanxu/musicplayer/MainActivity.java
@@ -1,7 +1,6 @@
 package science.duanxu.musicplayer;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
